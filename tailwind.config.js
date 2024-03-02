ccf457539d7762d24b707d5c70fbcc3caacbaba1
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-03-02 17:22:49 -0500

    Installed MaterialUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+const withMT = require("@material-tailwind/html/utils/withMT");
+ 
+module.exports = withMT({
+  content: ["./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+});
